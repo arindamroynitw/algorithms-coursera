5fed86fa534175e76fd0eec1d7b69adaf64c793c
Arindam Roy <arindamroy.roy619@gmail.com>
2019-03-28 14:30:46 +0530

    Quick Union

---
+++ dynamic-connectivity/src/QuickUnionUF.java
@@ -0,0 +1,30 @@
+public class QuickUnionUF {
+
+    private int[] id;
+
+    public QuickUnionUF(int n) {
+        id = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int proot = root(p);
+        int qroot = root(q);
+
+        id[proot] = qroot;
+    }
+}
